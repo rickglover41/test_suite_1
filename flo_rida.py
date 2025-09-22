a18e40de842a0d31edccd3dca191d87551e64709
Rick Glover <25041623+rickglover41@users.noreply.github.com>
2025-09-22 15:52:09 -0700

    Add files via upload

--- flo_rida.py
+++ flo_rida.py
@@ -137,7 +137,7 @@ with st.sidebar.expander("ℹ️ Data & Calculation Notes", expanded=False):
 			font-style: italic;
 			line-height: 1.5;">
 			<p>1. All rate and staffing information pulled from the Healthcare Cost Report Information System (HCRIS) FY2023. The reported Agency FTE use ({agency_fte}) was used to estimate the RN need (assuming 80% of the Agency FTEs were RNs working 1872 hours annually).</p>
-			<p>2. Estimated savings calculated using the current hospital staff labor rate plus the one-time Florence fee amortized over 3 years.</p>
+			<p>2. Estimated savings is a 3 year calculation, measured using the difference between the current reported agency hourly rate and the one-time Florence fee amortized over that 3 year period added to the current reported staff hourly rate.</p>
 		</div>
 		""",
 		unsafe_allow_html=True
